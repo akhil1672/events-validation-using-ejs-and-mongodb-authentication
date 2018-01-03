65d5e78e7f4ce2155c5eb6837972450e1cbbddd4
akhil1672 <33376520+akhil1672@users.noreply.github.com>
2018-01-03 19:43:11 +0530

    Major changes

--- expressejs.js
+++ expressejs.js
@@ -9,15 +9,18 @@ app.set('view engine','.ejs');
 app.use(bodyparser());
 let agevalidate = function(req,res,next){
     uname=req.body.uname;
+    pass1=req.body.pass1;
+    pass2=req.body.pass2;
     age=req.body.age;
     let obj = 
         {
             uname:uname,
+            pass:pass1,
             age:age
         }
     //console.log(age);
     //console.log(uname);
-    if(age<18 || !uname)
+    if(age<18 || !uname || !pass1 || !pass2 || (pass1!=pass2))
     {
         res.render('./events/ageerr');
     }
@@ -58,7 +61,7 @@ let loginvalidate = function(req,res,next)
                 }
                 else
                 {
-                    if(result.uname==uname)
+                    if(result.uname==uname && result.pass==pass)
                         res.render('./events/success',{name:uname});
                     else
                         res.render('./events/loginerr');
@@ -90,4 +93,4 @@ app.get('/signup',function(req,res){
 app.post('/signup', agevalidate, function (req1, res1) {
 
 })
-app.listen(3000,console.log("Listening on 3000"));
+app.listen(process.env.PORT||3000,console.log("Listening on 3000 "));
